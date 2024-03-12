fe4beff4dae81f06d3e8c7c02078b70cd9cdfc89
Diana Le <dianapnle@gmail.com>
2024-03-12 00:00:14 -0700

    updated model and migration with constraints

--- backend/db/migrations/20240312022703-create-user.js
+++ backend/db/migrations/20240312022703-create-user.js
@@ -26,7 +26,7 @@ module.exports = {
         unique: true
       },
       hashedPassword: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING.BINARY,
         allowNull: false
       },
       createdAt: {
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,58 +1,49 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
-  }
-  User.init({
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        findLength(value) {
-          if (value.length >= 4 && value.length <= 30) {
-            throw new Error ("must be at least 4 characters but less than 30")
-          }
-        },
-        isEmail: false
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        findLength(value) {
-          if (value.length >= 3 && value.length <= 256) {
-            throw new Error ("must be at least 3 characters and ")
-          }
-        },
-        isEmail: true
-      }
-    },
-    hashedPassword: {
-      type:DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        findLength(value) {
-          if (value.length === 60) {
-            throw new Error ("must be 60 characters")
+  };
+
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
           }
         }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
+        }
       }
+    }, {
+      sequelize,
+      modelName: 'User'
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  );
   return User;
 };
