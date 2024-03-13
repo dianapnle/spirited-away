2f03e2d75d3889b98dd87690db281dd4bdcd476d
Diana Le <dianapnle@gmail.com>
2024-03-13 13:32:55 -0700

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -44,9 +44,26 @@ if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
         user: safeUser
       });
 
-
-
   });
+
+  // Restore session user
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
 
   //Logout
 
