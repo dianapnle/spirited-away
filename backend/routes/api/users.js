cfec0f7270332e2407d95d53dd452919bfd8177c
Diana Le <dianapnle@gmail.com>
2024-03-13 16:07:42 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,12 +3,45 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+
+//The validateSignup middleware is composed of the check and handleValidationErrors middleware
+//   If at least one of the req.body values fail the check, an error will be returned as the response.
+const validateSignup = [
+    //It checks to see if req.body.email exists and is an email
+    check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+    //checks if req.body.username is a minimum length of 4 and is not an email
+    check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+    //checks if req.body.password is not empty and has a minimum length of 6
+    check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Must not be empty'),
+    check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Must not be empty'),
+    handleValidationErrors
+];
+
 // Sign up
-router.post('/', async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
     //extract email, password, and username from body
       const { email, password, firstName, lastName, username } = req.body;
       //then use bcrypt.hashsync to hash the user's provided password
@@ -31,7 +64,6 @@ router.post('/', async (req, res) => {
       });
     }
   );
-
 
 
 module.exports = router;
