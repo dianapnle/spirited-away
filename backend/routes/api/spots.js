0d5a95de9b194aeae091901036de29864008bab4
Diana Le <dianapnle@gmail.com>
2024-03-25 12:56:54 -0700

    spots init

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,5 @@
+//holds route paths to /api/session
+const express = require('express');
+const { Op } = require('sequelize');
+// const { User } = require('../../db/models');
+const router = express.Router();
