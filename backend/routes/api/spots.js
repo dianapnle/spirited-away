7a61038cbe5265b2e43be39953c686bf69b4518a
Diana Le <dianapnle@gmail.com>
2024-04-06 00:54:32 -0700

    more adjustments

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -80,21 +80,27 @@ const validateSpot = [
   check('country')
   .exists({ checkFalsy: true })
   .withMessage('Country is required'),
-  check('lat', 'Latitude must be within -90 and 90')
+  check('lat')
   .exists({ checkFalsy: true })
-  .isFloat({ min: -90 , max: 90}),
-  check('lng', 'Longitude must be within -180 and 180')
+  .withMessage('Latitude must be within -90 and 90')
+  .isFloat({ min: -90 , max: 90})
+  .withMessage('Latitude must be within -90 and 90'),
+  check('lng')
   .exists({ checkFalsy: true })
-  .isFloat( { min: -180 , max: 180}),
+  .withMessage('Longitude must be within -180 and 180')
+  .isFloat( { min: -180 , max: 180})
+  .withMessage('Longitude must be within -180 and 180'),
   check('name')
   .exists({ checkFalsy: true })
   .withMessage('Name must be less than 50 characters'),
   check('description')
   .exists({ checkFalsy: true })
   .withMessage('Description is required'),
-  check('price', 'Price per day must be a positive number')
+  check('price')
   .exists({ checkFalsy: true })
-  .isFloat( {min: 0}),
+  .withMessage('Price per day must be a positive number')
+  .isFloat( {min: 0})
+  .withMessage('Price per day must be a positive number'),
   handleValidationErrors
 ];
 
