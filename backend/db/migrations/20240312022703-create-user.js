09a2190894d06a5643eb2d2032331af84b73713e
Diana Le <dianapnle@gmail.com>
2024-04-05 18:38:09 -0700

    changes to deploy

--- backend/db/migrations/20240312022703-create-user.js
+++ backend/db/migrations/20240312022703-create-user.js
@@ -1,5 +1,7 @@
 'use strict';
+
 let options = {};
+
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
--- backend/db/seeders/20240325205335-spots.js
+++ backend/db/seeders/20240325205335-spots.js
@@ -2,7 +2,7 @@
 
 const { Spot } = require('../models');
 
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
